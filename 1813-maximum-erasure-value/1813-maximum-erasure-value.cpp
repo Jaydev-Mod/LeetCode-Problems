ab0ff4664fdc3092dadfc4adb19b5c831205a7a2
Jaydev Mod <modjaydev123@gmail.com>
2025-07-22 18:28:28 +0530

    Time: 0 ms (100%), Space: 94 MB (97.22%) - LeetHub

---
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maximumUniqueSubarray(vector<int>& nums) {
+        bool seen[10001] = {false};
+        int left = 0;
+        int currentSum = 0;
+        int maxSum = 0;
+
+        for (int right = 0; right < nums.size(); ++right) {
+            while (seen[nums[right]]) {
+                currentSum -= nums[left];
+                seen[nums[left]] = false;
+                ++left;
+            }
+            currentSum += nums[right];
+            seen[nums[right]] = true;
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+        }
+
+        return maxSum;
+    }
+};
