1905c17a9b6e6db9d59354fba9b5bb1378b27080
Jaydev Mod <modjaydev123@gmail.com>
2025-08-26 17:03:44 +0530

    Time: 1 ms (52.48%), Space: 21.3 MB (11.89%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
-    void bfs(int node, vector<vector<int>>& isConnected, vector<int>& visited) {
+    void bfs(int node, vector<vector<int>>& adjLs, vector<int>& visited) {
         queue<int> q;
         q.push(node);
         while (!q.empty()) {
             int temp = q.front();
             q.pop();
-            for(int i = 1; i <= isConnected.size(); i++){
-                if(isConnected[i-1][temp-1] == 1 && !visited[i]){
-                    visited[i] = 1;
-                    q.push(i);
+            for (int it : adjLs[temp]) {
+                if (!visited[it]) {
+                    visited[it] = 1;
+                    q.push(it);
                 }
             }
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
+        vector<vector<int>> adjLs(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isConnected[i][j] == 1 && i != j) {
+                    adjLs[i].push_back(j);
+                    adjLs[j].push_back(i);
+                }
+            }
+        }
         int count = 0;
-        vector<int> visited(n + 1, 0);
-        for (int i = 1; i <= n; i++) {
+        vector<int> visited(n, 0);
+        for (int i = 0; i < n; i++) {
             if (!visited[i]) {
-                bfs(i, isConnected, visited);
+                bfs(i, adjLs, visited);
                 count++;
             }
         }
