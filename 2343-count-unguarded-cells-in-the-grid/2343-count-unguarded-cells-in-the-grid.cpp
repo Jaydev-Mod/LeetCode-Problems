b091ba07fc8dd7f41b7be304105b5843f671e48c
Jaydev Mod <modjaydev123@gmail.com>
2025-11-02 23:43:48 +0530

    Time: 218 ms (20.64%), Space: 209.2 MB (25.23%) - LeetHub

---
+++ 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    void markGuarded(int row, int col, vector<vector<int>>& grid){
+        for(int i = row-1; i >= 0; i--){
+            if(grid[i][col] == 2 || grid[i][col]==3){
+                break;
+            }
+            grid[i][col] = 1;
+        }
+
+        for(int i = row+1; i < grid.size(); i++){
+            if(grid[i][col] == 2 || grid[i][col]==3){
+                break;
+            }
+            grid[i][col] = 1;
+        }
+
+        for(int i = col-1; i >= 0; i--){
+            if(grid[row][i] == 2 || grid[row][i]==3){
+                break;
+            }
+            grid[row][i] = 1;
+        }
+
+        for(int i = col+1; i < grid[0].size(); i++){
+            if(grid[row][i] == 2 || grid[row][i]==3){
+                break;
+            }
+            grid[row][i] = 1;
+        }
+
+        return;
+    }
+
+    int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
+        vector<vector<int>> grid(m, vector<int> (n, 0));
+        for(auto vec : guards){
+            int i = vec[0];
+            int j = vec[1];
+            grid[i][j] = 2;
+        }
+        for(auto vec : walls){
+            int i = vec[0];
+            int j = vec[1];
+            grid[i][j] = 3;
+        }
+
+        for(auto vec : guards){
+            int i = vec[0];
+            int j = vec[1];
+            markGuarded(i, j, grid);
+        }
+        int count = 0;
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j]==0){
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
